34116ab369686f7c27460ae99b3743b9cb094877
hitenderprakash <hitenderprakash@gmail.com>
2022-03-03 04:34:46 +0000

    Using test fixture

--- gtest/mathlib/test/src/test.cpp
+++ gtest/mathlib/test/src/test.cpp
@@ -5,37 +5,47 @@
 
 typedef double (*FPTR)(double,double);
 
-TEST(math_test, add){
-	void *dlhndl = dlopen("../libmath.so",RTLD_NOW);
-	FPTR fpnAdd = (FPTR)dlsym(dlhndl,"add");
+class math_test: public ::testing::Test{
+	protected:
+	
+	void *dlhndl;
+	FPTR fpnAdd;
+	FPTR fpnSubtract;
+	FPTR fpnMultiply;
+	FPTR fpnDivide;
+
+	void SetUp(){
+		dlhndl = dlopen("libmath.so",RTLD_NOW);	
+		fpnAdd = (FPTR)dlsym(dlhndl,"add");
+		fpnSubtract = (FPTR)dlsym(dlhndl,"subtract");
+		fpnMultiply = (FPTR)dlsym(dlhndl,"multiply");
+		fpnDivide = (FPTR)dlsym(dlhndl,"divide");
+	}
+	void TearDown(){
+		dlclose(dlhndl);
+	}
+};
+
+TEST_F(math_test, add){
 	ASSERT_EQ(fpnAdd(1,2),3);
-	dlclose(dlhndl);
 }
 
-TEST(math_test, subtract){
-	void *dlhndl = dlopen("../libmath.so",RTLD_NOW);
-	FPTR fpnSubtract = (FPTR)dlsym(dlhndl,"subtract");
+TEST_F(math_test, subtract){
 	ASSERT_EQ(fpnSubtract(4,2),2);
-	dlclose(dlhndl);
 }
-TEST(math_test, multiply){
-	void *dlhndl = dlopen("../libmath.so",RTLD_NOW);
-	FPTR fpnMultiply = (FPTR)dlsym(dlhndl,"multiply");
+
+TEST_F(math_test, multiply){
 	ASSERT_EQ(fpnMultiply(5,6),30);
-	dlclose(dlhndl);
 }
-TEST(math_test, divide){
-	void *dlhndl = dlopen("../libmath.so",RTLD_NOW);
-	FPTR fpnDivide = (FPTR)dlsym(dlhndl,"divide");
+
+TEST_F(math_test, divide){
 	ASSERT_EQ(fpnDivide(10,2),5);
-	dlclose(dlhndl);
 }
-TEST(math_test, divide2){
-	void *dlhndl = dlopen("../libmath.so",RTLD_NOW);
-	FPTR fpnDivide = (FPTR)dlsym(dlhndl,"divide");
+
+TEST_F(math_test, divide2){
 	ASSERT_EQ(fpnDivide(10,0),std::numeric_limits<double>::min());
-	dlclose(dlhndl);
 }
+
 int main(int argc, char **argv){
 	testing::InitGoogleTest(&argc,argv);
 	return RUN_ALL_TESTS();	
